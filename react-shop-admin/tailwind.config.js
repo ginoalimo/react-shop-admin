595341a3917b2cb782e78e445d11c4653a2e1fda
razurs <ginoalgarcia@gmail.com>
2022-03-07 21:27:56 -0500

    add page to index.js

---
+++ react-shop-admin/tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*{html,js,jsx}"],
+  content: [],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+};
